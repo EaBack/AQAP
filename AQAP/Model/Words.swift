c3683560f90c25aef49be7f3e3b159fb01b51011
EaBack <113344913+EaBack@users.noreply.github.com>
2023-03-31 15:09:55 +0200

    borjat om

---
+++ AQAP/Model/Words.swift
@@ -0,0 +1,92 @@
+//
+//  Words.swift
+//  AQAP
+//
+//  Created by Edgar Backer on 2023-03-31.
+//
+
+import Foundation
+
+class Words{
+    
+    var listOfWords = [
+        "detta",
+       "genom",
+           "några",
+            "eller",
+            "andra",
+            "vilken",
+            "deras",
+            "kommer",
+           "varje",
+            "berätta",
+            "uppsättning",
+            "också",
+            "spela",
+            "liten",
+            "sätta",
+            "sidan",
+            "stava",
+            "sådan",
+            "handling",
+            "varför",
+            "förändring",
+           "behöver",
+           "bilden",
+           "försök",
+           "punkt",
+            "världen",
+           "bygga",
+           "själv",
+           "någon",
+           "arbete",
+           "delen",
+           "plats",
+           "gjort",
+           "lever",
+           "efter",
+           "tillbaka",
+           "liten",
+           "endast",
+           "människa",
+           "visar",
+           "varje",
+           "enligt",
+           "mycket",
+           "genom",
+           "formulär",
+            "meningen",
+            "hjälpa",
+           "linje",
+            "mycket",
+           "betyda",
+           "höger",
+            "pojke",
+           "gammal",
+          "alltför",
+           "samma",
+            "använda",
+           "många",
+           "cykelverkstad",
+           "skriva",
+            "skulle",
+            "liknande",
+           "dessa",
+           "hennes",
+           "honom",
+            "kunde",
+            "komma",
+           "gjorde",
+           "antal",
+           "människor",
+           "vatten",
+            "samtal",
+           "först",
+            "varit",
+           "hitta"]
+    
+    
+    init(){
+        
+    }
+}
